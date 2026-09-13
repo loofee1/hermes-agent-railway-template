e0c0e651e5729e6134158f3b67fe358bd264e0bf
loofee1 <loofee.11@gmail.com>
2026-09-14 02:28:32 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
+# Basic Auth intentionally disabled; Nous OAuth only.
+
 export HERMES_DASHBOARD_PORT="${HERMES_DASHBOARD_PORT:-${PORT:-8080}}"
 
 exec /opt/hermes/docker/entrypoint-dispatch.sh "$@"
